46d73765bf89f597b7614d81a1706b312dfd3247
LUHANBIN <769527098@qq.com>
2016-03-09 22:47:05 +0800

    556

--- canvas_oop/canvas_oop.js
+++ canvas_oop/canvas_oop.js
@@ -59,11 +59,12 @@ var TextField = (function (_super) {
     __extends(TextField, _super);
     function TextField() {
         _super.apply(this, arguments);
+        this.T = "";
     }
     TextField.prototype.render = function (context) {
-        context.font = "20px Arial";
+        context.font = "36px Arial";
         context.fillStyle = '#000000';
-        context.fillText('HelloWorld', 0, 20);
+        context.fillText(this.T, 0, 20);
     };
     return TextField;
 }(DisplayObject));
@@ -96,24 +97,46 @@ function loadResource(imageList, callback) {
 var canvas = document.getElementById("game");
 var context = canvas.getContext("2d");
 var rect = new Rect();
-rect.width = 200;
-rect.height = 100;
-rect.color = '#00FF00';
+rect.width = 800;
+rect.height = 400;
+rect.color = '#14e3cd';
 var rect2 = new Rect();
-rect2.width = 300;
-rect2.height = 50;
-rect2.x = 200;
-rect2.y = 200;
-rect2.rotation = Math.PI / 8;
-rect2.color = '#00FFFF';
+rect2.width = 800;
+rect2.height = 100;
+rect2.x = 0;
+rect2.y = 300;
+//rect2.rotation = Math.PI / 8;
+rect2.color = '#31e314';
+var rect3 = new Rect();
+rect3.width = 50;
+rect3.height = 50;
+rect3.x = 50;
+rect3.y = 300;
+rect3.color = '#dce314';
+var rect4 = new Rect();
+rect4.width = 100;
+rect4.height = 50;
+rect4.x = 200;
+rect4.y = 300;
+rect4.color = '#dce314';
+var rect5 = new Rect();
+rect5.width = 50;
+rect5.height = 50;
+rect5.x = 500;
+rect5.y = 300;
+rect5.color = '#dce314';
 var text = new TextField();
 text.x = 10;
+text.y = 200;
+text.T = "Play";
 var bitmap = new Bitmap();
-bitmap.source = 'wander-icon.jpg';
+bitmap.source = '13081161.png';
+bitmap.x = 400;
+bitmap.y = 200;
 //渲染队列
 var renderQueue = [rect, rect2, text, bitmap];
 //资源加载列表
-var imageList = ['wander-icon.jpg'];
+var imageList = ['13081161.png'];
 //先加载资源，加载成功之后执行渲染队列
 loadResource(imageList, function () {
     drawQueue(renderQueue);
