1479d0dc6642d40fd852d7b6714650af750b4893
LUHANBIN <769527098@qq.com>
2016-03-17 12:14:02 +0800

    TEST

---
+++ animation/animation.ts
@@ -0,0 +1,103 @@
+/**
+ * 重力加速度
+ */
+var GRAVITY = 9.8;
+var BOUNDS_BOTTOM = 400;
+var BOUNDS_LEFT = 0;
+var BOUNDS_RIGHT = 400;
+var BOUNCE = 0.95;
+var F=-0.8;
+/**
+ * 计时器系统
+ */
+class Ticker {
+
+    bodyQueue = [];
+
+    lastTime;
+
+    /**
+     * 启动计时器
+     * @param bodyList 物理队列
+     */
+    start(bodyQueue) {
+        this.bodyQueue = bodyQueue;
+        this.lastTime = Date.now();
+        var self = this;
+        setInterval(this.onTicker.bind(this), 1000 / 60);
+    }
+
+    onTicker() {
+        var currentTime = Date.now();
+        var duringTime = currentTime - this.lastTime;
+        this.lastTime = currentTime;
+        this.bodyQueue.map(function(body) {
+            body.onTicker(duringTime / 100)
+        });
+    }
+}
+
+
+var Body = (function () {
+    function Body(displayObject) {
+        this.vx = 0;
+        this.vy = 0;
+        this.x = 0;
+        this.y = 0;
+        this.width = 0;
+        this.height = 0;
+        this.displayObject = displayObject;
+    }
+Body.prototype.onTicker = function (duringTime) {
+
+        this.vy += duringTime * GRAVITY;
+        this.x += duringTime * this.vx;
+        this.y += duringTime * this.vy;
+
+        //反弹
+        if (this.y + this.height > BOUNDS_BOTTOM) {
+            this.vy = -BOUNCE * this.vy;
+            this.y = BOUNDS_BOTTOM - this.height;
+        }
+        /*  if(this.vy=duringTime * this.vy){
+   
+               this.vx=F*this.vx;
+           }*/
+
+        //TODO： 左右越界反弹
+if ((this.x + this.width > BOUNDS_RIGHT) || (this.x < BOUNDS_LEFT)) {
+            this.vx = -BOUNCE * this.vx;
+        }
+        /*   if(this.y+this.height > BOUNDS_BOTTOM ){
+               this.vx=F*this.vx;
+           }*/
+        //根据物体位置更新显示对象属性
+        var displayObject = this.displayObject;
+        displayObject.x = this.x;
+        displayObject.y = this.y;
+    };
+     return Body;
+}());
+var rect = new Rect();
+rect.width = 150;
+rect.height = 100;
+rect.color = '#FF0000';
+
+/**
+ * 创建一个物体，其显示内容为一个长方形，受重力做平抛运动
+ */
+var body = new Body(rect);
+body.width = rect.width;
+body.height = rect.height;
+body.vx = 60;//需要保证 vx 在 0-50的范围内行为正常
+body.vy = 0;//需要保证 vy 在 0-50的范围内行为正常
+//DEBUG
+console.debug();
+//DEBUG END
+var renderCore = new RenderCore();
+var ticker = new Ticker();
+
+renderCore.start([rect]);
+ticker.start([body]);
+
+
